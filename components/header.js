510a2587b8e8c424429609732f872bfb970a31d3
daiyaoa <daiyaoa@enn.cn>
2023-08-07 17:17:01 +0800

    perf: 改版头部

--- components/header.js
+++ components/header.js
@@ -1,49 +1,33 @@
 import styles from './header.module.css';
 import HeaderText from './HeaderText';
-import Border from './border';
 import Image from 'next/image';
 import Button from '@mui/material/Button';
+
 export default function Header() {
   return (
     <div className={styles.navContainer}>
-      <div className={styles.headerUp}>
-        <div className={styles.column}>
+      <div className={styles.topNavigation}>
+        <div className={styles.logo}>
           <Image
             className={styles.borderCircle}
             src="/profile.jfif"
-            height={80} // Desired size with correct aspect ratio
-            width={80} // Desired size with correct aspect ratio
+            height={50} // Desired size with correct aspect ratio
+            width={50} // Desired size with correct aspect ratio
             alt="George"
-            fill={false}
           />
+          <p>George Weblog</p>
         </div>
-        <div className={styles.column}>
-          <div className={styles.buttonLocation}>
-            <Button
-              disabled={false}
-              size="large"
-              color="secondary"
-              variant="outlined"
-              style={{
-                color: 'black',
-                borderColor: 'black',
-                borderRadius: '20px',
-              }}
-            >
-              Subscribe
-            </Button>
-          </div>
-        </div>
+        <Button disabled={false} size="medium" color="secondary" variant="outlined">
+          Subscribe
+        </Button>
       </div>
-      <Border></Border>
       <div className={styles.nav}>
         <HeaderText title={'Home Page'} path={'/'}></HeaderText>
         <HeaderText title={'Blogs'} path={'/posts/Nodejs-Eggjs'}></HeaderText>
         <HeaderText title={'Small Business Tips'}></HeaderText>
         <HeaderText title={'Inspiration'}></HeaderText>
-        <HeaderText title={'Resources'}></HeaderText>
+        <HeaderText title={'1'}></HeaderText>
       </div>
-      <Border></Border>
     </div>
   );
 }
