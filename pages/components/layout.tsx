d90935332c86fe4f5f74e9d53d91af8e9cc39709
George Charles <georgecharles1993@outlook.com>
2024-01-18 01:01:48 +0800

    fix: export deafult

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -23,3 +23,5 @@ export function Layout({ children }) {
 Layout.propTypes = {
   children: propTypes.any,
 };
+
+export default Layout;
--- pages/upload/model.js
+++ pages/upload/model.js
@@ -68,3 +68,5 @@ export function downloadImage() {
     }
   );
 }
+
+export default deletePost;
